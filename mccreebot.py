12e1236687b44dce27bfbd431af47da2f847aa5d
ajo238 <ade72598@gmail.com>
2020-12-16 13:46:09 +0100

    Bot is finished, must be tested on a server for a few days

--- mccreebot.py
+++ mccreebot.py
@@ -1,10 +1,9 @@
 import os
-
 import discord
+from discord.ext import tasks, commands 
 import datetime
+import asyncio
 
-now = datetime.datetime.now()
-clock = str(datetime.time(now.hour, now.minute, now.second))
 
 TOKEN = open("/home/adrian/projects/mccreebottoken").readline() 
 GUILD =  'Team Avatar'
@@ -15,44 +14,37 @@ async def on_ready():
     for guild in client.guilds:
         if guild.name == GUILD:
             break
+    await client.change_presence(activity=discord.Game('Overwatch'))
 
     print(
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
-"""
-@client.event
-async def message(message):
-    print("nani")
-    
-    # TODO: Find a way to create a new message on a given time 
-    message = discord.Message(state=0,channel=discord.TextChannel,data=None)
-    
-    if clock == "12:00:00":
-        txt = "It's High Noon!!!"
-        await message.channel.send(txt)
-"""
+
+async def send_message():
+    await client.wait_until_ready()
+    print("Entered loop") 
+    while not client.is_closed(): 
+        now = datetime.datetime.now()
+        clock = str(datetime.time(now.hour, now.minute))
+        if clock == "12:00":
+            print("It's "+str(now.hour)+":"+str(now.minute))
+            txt = "It's High Noon!!!"
+            channel = client.get_channel(id=785975501470695505)   
+            await channel.send(txt,file=discord.File('highnoon.jpg'))
+            await asyncio.sleep(120)
+        else:
+            await asyncio.sleep(15)
+            continue
+
 @client.event
 async def on_message(message):
     if message.author.id == client.user.id:
         return
     #message starts with hi or hello then print these
-    if message.content.startswith('Hi') or message.content.startswith('Hello'):
-        await message.channel.send('Ah hello {0.author.mention} u bitch'.format(message))
-    #when the message with help then do this
-    elif message.content.startswith('help'):
-        await message.channel.send("Git gud")
+    if message.content.startswith('Is it High Noon?') or message.content.startswith('high noon?') or message.content.startswith('highnoon?'):
+        await message.channel.send('Its High Noon somewhere in the world {0.author.mention} '.format(message))
 
-    elif message.content.startswith('You blastard'):
-        await message.channel.send('no but it why')
-    elif message.content.startswith('if you go accident what u gonna do'):
-        await message.channel.send('WHY YOU FUCK ME I FUCK YOU BLOODY')
-        await message.channel.send('Bloody blastard')
-    elif message.content.startswith('Bye') or message.content.startswith('Goodbye'):
-        await message.channel.send('Bye have a nice day')
-    elif message.content.startswith('Man why am I talking to a bot?'):
-        await message.channel.send('Cus ur 4everalone #getrekt')
-
-
+client.loop.create_task(send_message())
 client.run(TOKEN)
